472061b22fa0d113124424a4818f2d8a0f2d72f9
dheker <dhekerlaadhibii@gmail.com>
2024-02-22 22:28:56 +0100

    step 1

--- project.java
+++ project.java
@@ -1,9 +1,22 @@
+import java.util.Random;
+
 /**
  * project
  */
 public class project {
 
     public static void main(String[] args) {
-        System.out.println("heelo");
+        // Create a new instance of the Random class
+        Random r = new Random();
+        // Generate a random number between 0 (inclusive) and 200 (inclusive) and assign
+
+        // it to the variable FindMe
+        int Randomly = r.nextInt(201);
+        int prix = 0;
+
+        while (Randomly != prix) {
+System.out.println("please enter a number : between  0 & 200 ! ");
+        }
+        System.out.println("Yesss champion you did it !!!!" + Randomly);
     }
 }
